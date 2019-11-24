05654060d3a46fee3875b1ce80a8f99397e3bde6
Lucie Fontela <luciefontela@gmail.com>
2019-11-24 15:31:11 +0100

    button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -12,6 +12,12 @@ function Counter() {
         <div>
           <p>Number</p>
           <input value={count}onChange={handleStatusCount}/>
+          <button onClick={() => setCount(count + 1)}>
+          +1
+        </button>
+        <button onClick={() => setCount(count - 1)}>
+          -1
+        </button>
         </div>
       )
     }
